f5c539ab26c38e204653a524be4bc17f6dc44b8f
hm-shahadat <shahadat.22.48095.2.aiub.edu@gmail.com>
2026-09-24 15:44:26 +0600

    Done

--- problemTwo.go
+++ problemTwo.go
@@ -1,5 +1,9 @@
-package main
+// Make a simple function called greet that returns the most-famous "hello world!".
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
 
-func main() {
+package kata // do not remove
 
+func greet() string {
+return "hello world!"
 }
